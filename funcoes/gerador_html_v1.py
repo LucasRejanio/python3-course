964102063520a54dc35b7450fc263260d417647b
LucasRejanio <lucas.rejanio@gmail.com>
2021-02-25 21:35:39 -0300

    Upload new files

---
+++ funcoes/gerador_html_v1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def tag_bloco(texto, classe='sucess'):
+    return f'<div class="{classe}">{texto}</div>'
+
+if __name__ == '__main__':
+    print(f'Resultado:', tag_bloco('Incluiído com sucesso'))
