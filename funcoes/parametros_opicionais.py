a3cbe2a490df28161053b46d5b167203132b0490
LucasRejanio <lucas.rejanio@gmail.com>
2021-03-22 15:51:58 -0300

    Example parameters optionals

---
+++ funcoes/parametros_opicionais.py
@@ -0,0 +1,9 @@
+from typing import Optional, Mapping
+
+
+def test_optional(string: Optional[str] = None,
+                  inteiro: Optional[int] = None,
+                  real: Optional[float] = None,
+                  booleano: Optional[bool] = None,
+                  lista: Optional[Mapping[str, str]] = None,):
+                  pass
